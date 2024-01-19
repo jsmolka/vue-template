14d9e58dfd4d38633d145f4f0ace8b64c31b03fb
jsmolka <jsmolka@pm.me>
2024-01-19 12:20:34 +0100

    update

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="flex justify-center items-center h-full">
-    <span class="text-gray-1 font-black">Index</span>
+    <Button brand>Index</Button>
   </div>
 </template>
+
+<script setup>
+import Button from '../components/Button.vue';
+</script>
