e072554258b2c901f6d3f394ad6d46b988aa531a
jsmolka <jsmolka@pm.me>
2024-05-31 20:57:29 +0200

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,23 @@
 import vue from '@vitejs/plugin-vue';
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 
+function relative(path) {
+  return fileURLToPath(new URL(path, import.meta.url));
+}
+
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [autoprefixer, tailwindcss],
+    },
+  },
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': relative('./src'),
+    },
+  },
 });
