87a6f47ae2f4b87937d113da19d5f815f70b84ec
jsmolka <jsmolka@pm.me>
2024-06-01 21:00:26 +0200

    add button

---
+++ src/components/ui/button/Button.vue
@@ -0,0 +1,65 @@
+<template>
+  <Primitive
+    :as="as"
+    :as-child="asChild"
+    :class="cn(buttonVariants({ variant, size }), props.class)"
+  >
+    <slot />
+  </Primitive>
+</template>
+
+<script setup>
+import { cn } from '@/utils/ui';
+import { cva } from 'class-variance-authority';
+import { Primitive } from 'radix-vue';
+
+const props = defineProps({
+  variant: {
+    type: null,
+    required: false,
+  },
+
+  size: {
+    type: null,
+    required: false,
+  },
+
+  as: {
+    type: null,
+    required: false,
+    default: 'button',
+  },
+
+  asChild: {
+    type: Boolean,
+    required: false,
+  },
+
+  class: {
+    type: null,
+    required: false,
+  },
+});
+
+const buttonVariants = cva(
+  'inline-flex justify-center items-center text-sm font-medium whitespace-nowrap rounded-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-brand-3 disabled:pointer-events-none disabled:opacity-50',
+  {
+    variants: {
+      variant: {
+        default: 'bg-brand-3 text-shade-8 hover:bg-brand-2',
+        secondary: 'bg-shade-6 text-shade-2 hover:bg-shade-5',
+        outline: 'bg-shade-8 text-shade-2 border border-shade-6 hover:bg-shade-7',
+        ghost: 'text-shade-2 hover:bg-shade-7',
+      },
+      size: {
+        default: 'h-8 px-4 py-2',
+        icon: 'size-8',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  },
+);
+</script>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,9 +1,34 @@
 <template>
-  <div class="flex justify-center items-center h-full">
-    <Button brand>Index</Button>
+  <div class="flex flex-col gap-4 p-4 mx-auto max-w-sm">
+    <h1>Button</h1>
+    <div class="grid grid-cols-2 gap-2">
+      <Button variant="default">Default</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button disabled variant="default">Default</Button>
+      <Button disabled variant="secondary">Secondary</Button>
+      <Button disabled variant="outline">Outline</Button>
+      <Button disabled variant="ghost">Ghost</Button>
+      <Button variant="outline" size="icon">
+        <DotsVerticalIcon />
+      </Button>
+      <Button variant="ghost" size="icon">
+        <DotsVerticalIcon />
+      </Button>
+    </div>
   </div>
 </template>
 
 <script setup>
-import Button from '@/components/Button.vue';
+import { Button } from '@/components/ui/button';
+import { DotsVerticalIcon } from '@radix-icons/vue';
 </script>
+
+<style scoped>
+h1 {
+  @apply text-lg;
+  @apply text-shade-1;
+  @apply font-semibold;
+}
+</style>
