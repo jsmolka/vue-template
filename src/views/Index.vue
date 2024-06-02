f0346400bc41d7061094ac7aa2e3fd97d0d9d6a0
jsmolka <jsmolka@pm.me>
2024-06-02 21:17:23 +0200

    improve index page and components

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,149 +1,170 @@
 <template>
-  <div class="flex flex-col gap-4 p-4 mx-auto max-w-sm">
-    <Logo class="size-8 self-center" />
+  <div class="grid gap-4 max-w-sm mx-auto p-4">
+    <Logo class="place-self-center size-8 text-shade-1" />
 
-    <h1>Button</h1>
     <div class="grid gap-2">
+      <h1>Button</h1>
       <div class="grid grid-cols-2 gap-2">
         <template v-for="variant in ['default', 'secondary', 'outline', 'ghost']">
           <Button v-for="disabled in [false, true]" :variant="variant" :disabled="disabled">
-            <span class="capitalize">{{ variant }}</span>
+            Button
           </Button>
         </template>
-      </div>
-      <div class="flex gap-2">
-        <template v-for="variant in ['outline', 'ghost']">
-          <Button :variant="variant" size="icon">
+        <div v-for="disabled in [false, true]" class="grid grid-cols-4 gap-2">
+          <Button
+            v-for="variant in ['default', 'secondary', 'outline', 'ghost']"
+            :variant="variant"
+            :disabled="disabled"
+            size="icon"
+          >
             <DotsVerticalIcon />
           </Button>
-        </template>
+        </div>
       </div>
     </div>
 
-    <h1>Checkbox</h1>
-    <div class="flex gap-2">
-      <Checkbox :model-value="false" />
-      <Checkbox :model-value="true" />
-      <Checkbox :model-value="true" disabled />
-    </div>
-
-    <h1>Dropdown Menu</h1>
-    <DropdownMenu>
-      <DropdownMenuTrigger as-child>
-        <Button variant="outline">Open</Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuGroup>
-          <DropdownMenuItem>
-            <span>Item</span>
-            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <span>Item</span>
-            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuSub>
-            <DropdownMenuSubTrigger>Submenu</DropdownMenuSubTrigger>
-            <DropdownMenuPortal>
-              <DropdownMenuSubContent>
-                <DropdownMenuItem>
-                  <span>Item</span>
-                  <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <span>Item</span>
-                  <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-                </DropdownMenuItem>
-              </DropdownMenuSubContent>
-            </DropdownMenuPortal>
-          </DropdownMenuSub>
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuLabel>Checkbox</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuCheckboxItem checked>Value</DropdownMenuCheckboxItem>
-          <DropdownMenuCheckboxItem>Value</DropdownMenuCheckboxItem>
-          <DropdownMenuCheckboxItem>Value</DropdownMenuCheckboxItem>
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuLabel>Radio Group</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuRadioGroup model-value="1">
-            <DropdownMenuRadioItem value="1">Value 1</DropdownMenuRadioItem>
-            <DropdownMenuRadioItem value="2">Value 2</DropdownMenuRadioItem>
-            <DropdownMenuRadioItem value="3">Value 3</DropdownMenuRadioItem>
-          </DropdownMenuRadioGroup>
-        </DropdownMenuGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
-
-    <h1>Input</h1>
-    <div class="flex flex-col gap-2">
-      <Input placeholder="Input" />
-      <InputNumber
-        placeholder="Input number"
-        :min="-10000"
-        :max="+10000"
-        :precision="2"
-        :unit="'m'"
-      />
-    </div>
-
-    <h1>Label</h1>
     <div class="grid gap-2">
+      <h1>Checkbox</h1>
+      <div class="grid grid-cols-4 gap-2">
+        <Checkbox :model-value="true" />
+        <Checkbox :model-value="true" disabled />
+        <Checkbox />
+        <Checkbox disabled />
+      </div>
+    </div>
+
+    <div class="grid gap-2">
+      <h1>Dropdown Menu</h1>
+      <DropdownMenu>
+        <DropdownMenuTrigger as-child>
+          <Button variant="secondary">Open</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              <span>Item</span>
+              <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+            </DropdownMenuItem>
+            <DropdownMenuItem disabled>
+              <span>Item</span>
+              <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+            </DropdownMenuItem>
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>Submenu</DropdownMenuSubTrigger>
+              <DropdownMenuPortal>
+                <DropdownMenuSubContent>
+                  <DropdownMenuItem>
+                    <span>Item</span>
+                    <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem disabled>
+                    <span>Item</span>
+                    <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuLabel>Checkbox</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuCheckboxItem checked>Value</DropdownMenuCheckboxItem>
+            <DropdownMenuCheckboxItem>Value</DropdownMenuCheckboxItem>
+            <DropdownMenuCheckboxItem disabled>Value</DropdownMenuCheckboxItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuLabel>Radio Group</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuRadioGroup model-value="1">
+              <DropdownMenuRadioItem value="1">Value 1</DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="2">Value 2</DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="3" disabled>Value 3</DropdownMenuRadioItem>
+            </DropdownMenuRadioGroup>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+
+    <div class="grid gap-2">
+      <h1>Input</h1>
+      <div class="grid grid-cols-2 gap-2">
+        <Input placeholder="Input" />
+        <Input placeholder="Input" disabled />
+        <InputNumber placeholder="Input number" :precision="2" unit="mm" />
+        <InputNumber placeholder="Input number" :precision="2" unit="mm" disabled />
+      </div>
+    </div>
+
+    <div class="grid gap-2">
+      <h1>Label</h1>
       <Label>Length</Label>
       <InputNumber :model-value="100" unit="mm" />
     </div>
 
-    <h1>Select</h1>
-    <div class="flex gap-2">
-      <Select :items="[1, 2, 3]" placeholder="Select">
-        <template #item="{ item }">
-          <SelectItemText>Value {{ item }}</SelectItemText>
-        </template>
-      </Select>
-      <Select :items="[]" placeholder="Select" disabled />
+    <div class="grid gap-2">
+      <h1>Select</h1>
+      <div class="grid grid-cols-2 gap-2">
+        <Select
+          v-for="disabled in [false, true]"
+          :items="[1, 2, 3]"
+          placeholder="Select"
+          :disabled="disabled"
+        >
+          <template #item="{ item }">
+            <span class="flex items-center gap-2">
+              <CookieIcon />
+              <SelectItemText>Value {{ item }}</SelectItemText>
+            </span>
+          </template>
+        </Select>
+      </div>
     </div>
 
-    <h1>Switch</h1>
     <div class="grid gap-2">
-      <div class="flex gap-2">
+      <h1>Switch</h1>
+      <div class="grid grid-cols-4 gap-2">
         <Switch :model-value="true" />
         <Switch :model-value="true" disabled />
-      </div>
-      <div class="flex gap-2">
-        <Switch :model-value="false" />
-        <Switch :model-value="false" disabled />
+        <Switch />
+        <Switch disabled />
       </div>
     </div>
 
-    <h1>Tabs</h1>
-    <Tabs default-value="1">
-      <TabsList class="grid grid-cols-2">
-        <TabsTrigger value="1">One</TabsTrigger>
-        <TabsTrigger value="2">Two</TabsTrigger>
-      </TabsList>
-      <TabsContent value="1">
-        <div class="border rounded-sm p-2">Content 1</div>
-      </TabsContent>
-      <TabsContent value="2">
-        <div class="border rounded-sm p-2">Content 2</div>
-      </TabsContent>
-    </Tabs>
+    <div class="grid gap-2">
+      <h1>Tabs</h1>
+      <Tabs default-value="1">
+        <TabsList class="grid grid-cols-3">
+          <TabsTrigger value="1">Tab 1</TabsTrigger>
+          <TabsTrigger value="2">Tab 2</TabsTrigger>
+          <TabsTrigger value="3" disabled>Tab 3</TabsTrigger>
+        </TabsList>
+        <TabsContent value="1">
+          <div class="border rounded-sm p-2">Tab content 1</div>
+        </TabsContent>
+        <TabsContent value="2">
+          <div class="border rounded-sm p-2">Tab content 2</div>
+        </TabsContent>
+        <TabsContent value="3">
+          <div class="border rounded-sm p-2">Tab content 3</div>
+        </TabsContent>
+      </Tabs>
+    </div>
 
-    <h1>Tooltip</h1>
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger as-child>
-          <Button variant="secondary">Hover</Button>
-        </TooltipTrigger>
-        <TooltipContent>
-          <p>Content</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <div class="grid gap-2">
+      <h1>Tooltip</h1>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger as-child>
+            <Button variant="secondary">Hover</Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Content</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
   </div>
 </template>
 
@@ -174,7 +195,7 @@ import { Select, SelectItemText } from '@/components/ui/select';
 import { Switch } from '@/components/ui/switch';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
-import { DotsVerticalIcon } from '@radix-icons/vue';
+import { CookieIcon, DotsVerticalIcon } from '@radix-icons/vue';
 </script>
 
 <style scoped>
