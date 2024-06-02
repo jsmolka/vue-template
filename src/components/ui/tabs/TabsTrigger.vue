01c9ee6183dc62e6e7783f0481e8aa53e7e7d232
jsmolka <jsmolka@pm.me>
2024-06-02 22:08:39 +0200

    formatting

--- src/components/ui/tabs/TabsTrigger.vue
+++ src/components/ui/tabs/TabsTrigger.vue
@@ -3,7 +3,7 @@
     v-bind="forwardedProps"
     :class="
       cn(
-        'inline-flex justify-center items-center h-8 whitespace-nowrap rounded-sm px-4 py-2 text-sm font-medium transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-brand-3 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-shade-6 data-[state=active]:text-shade-2',
+        'inline-flex justify-center items-center h-8 px-4 py-2 text-sm font-medium rounded-sm whitespace-nowrap transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-brand-3 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-shade-6 data-[state=active]:text-shade-2',
         props.class,
       )
     "
