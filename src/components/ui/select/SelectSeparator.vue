7aa411f254c2748cb64e5c122369c6210f47166e
jsmolka <jsmolka@pm.me>
2024-06-05 09:49:11 +0200

    add select group, label and separator

---
+++ src/components/ui/select/SelectSeparator.vue
@@ -0,0 +1,22 @@
+<template>
+  <SelectSeparator v-bind="delegatedProps" :class="cn('h-px -mx-1 my-1 bg-muted', props.class)" />
+</template>
+
+<script setup>
+import { cn } from '@/utils/ui';
+import { SelectSeparator } from 'radix-vue';
+import { computed } from 'vue';
+
+let props;
+props = defineProps({
+  as: { required: false },
+  asChild: { type: Boolean, required: false },
+  class: { required: false },
+});
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props;
+
+  return delegated;
+});
+</script>
