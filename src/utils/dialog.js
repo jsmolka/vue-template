e3fad7c9ccef0fe537b48017b3bd6ecd778abfd3
jsmolka <jsmolka@pm.me>
2024-06-07 11:10:22 +0200

    support nested dialogs

--- src/utils/dialog.js
+++ src/utils/dialog.js
@@ -1,11 +1,25 @@
 import Dialog from '@/utils/Dialog.vue';
 import { createApp } from 'vue';
 
-const div = document.createElement('div');
-document.body.appendChild(div);
+function mount() {
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+  const app = createApp(Dialog);
 
-const component = createApp(Dialog).mount(div);
+  return {
+    dialog: app.mount(div),
+    unmount() {
+      app.unmount();
+      div.remove();
+    },
+  };
+}
 
 export async function dialog(content, buttons) {
-  return component.open(content, buttons);
+  const { dialog, unmount } = mount();
+  try {
+    return await dialog.open(content, buttons);
+  } finally {
+    unmount();
+  }
 }
