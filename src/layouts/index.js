1c5a7c712699858b7478673d9b0086edcb4f39b7
jsmolka <jsmolka@pm.me>
2024-06-30 15:10:02 +0200

    use default layout for base routes

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,13 +1,26 @@
-export function setupLayouts(routes) {
+function setupLayoutsRecursive(routes, isBase) {
   return routes.map((route) => {
     if (route.children?.length > 0) {
-      route.children = setupLayouts(route.children);
+      route.children = setupLayoutsRecursive(route.children, false);
     }
 
-    return {
-      path: route.path,
-      component: route.meta?.layout ?? (() => import('@/layouts/default/Index.vue')),
-      children: [{ ...route, path: '' }],
-    };
+    let layout = route.meta?.layout;
+    if (layout == null && isBase) {
+      layout = () => import('@/layouts/default/Index.vue');
+    }
+
+    if (layout) {
+      return {
+        path: route.path,
+        component: layout,
+        children: [{ ...route, path: '' }],
+      };
+    }
+
+    return route;
   });
+}
+
+export function setupLayouts(routes) {
+  return setupLayoutsRecursive(routes, true);
 }
