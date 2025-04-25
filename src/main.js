127a216083984ba6453c3d18bdefea6e99e5ae17
jsmolka <jsmolka@pm.me>
2025-04-25 10:26:02 +0200

    use css instead of scss

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import App from '@/App.vue';
-import '@/main.scss';
+import '@/main.css';
 import { router } from '@/router';
 import { useStores } from '@/stores';
 import { createPinia } from 'pinia';
