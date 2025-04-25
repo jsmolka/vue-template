ff99ad430b0473336f7cd52a14a84a8aeb3fcdff
jsmolka <jsmolka@pm.me>
2025-04-25 10:27:05 +0200

    remove uppercase from label

--- src/components/ui/label/Label.vue
+++ src/components/ui/label/Label.vue
@@ -3,7 +3,7 @@
     v-bind="forwardedProps"
     :class="
       cn(
-        'text-xs font-medium uppercase leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
+        'text-xs font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
         props.class,
       )
     "
