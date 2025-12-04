58bf1a7924fc708a4cb467b98bc35220a54d5196
jsmolka <jsmolka@pm.me>
2025-12-04 20:32:57 +0100

    add pwa support

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,35 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [vue(), vueJsx(), tailwindcss()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'vue-template',
+        description: 'vue-template-description',
+        theme_color: '#242933',
+        background_color: '#242933',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
