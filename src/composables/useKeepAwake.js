d6c05ba115d52817374510348ab87d47e7cbdae5
jsmolka <jsmolka@pm.me>
2025-12-05 08:34:49 +0100

    add wake lock

---
+++ src/composables/useKeepAwake.js
@@ -0,0 +1,12 @@
+import { useDocumentVisibility, useEventListener, useWakeLock, whenever } from '@vueuse/core';
+
+export function useKeepAwake() {
+  const { request } = useWakeLock();
+  const visibility = useDocumentVisibility();
+
+  const keepAwake = () => {
+    whenever(() => visibility.value === 'visible', request, { immediate: true });
+  };
+
+  useEventListener(window, 'click', keepAwake, { once: true });
+}
